e53e42cca2bccb6f1632f7aa8875acf1afb6fcb1
Valooon <valentinososas27@gmail.com>
2020-12-02 16:17:08 -0300

    Commit message

--- routes/bd.js
+++ routes/bd.js
@@ -2,9 +2,9 @@ var mysql=require('mysql');
 
 var conexion=mysql.createConnection({
     host:'remotemysql.com', 
-   user:'35S0HfTPFE', 
-   password:'18HwyinR26', 
-   database:'35S0HfTPFE' 
+    user:'35S0HfTPFE', 
+    password:'18HwyinR26', 
+    database:'35S0HfTPFE' 
 });
 
 conexion.connect(function (error){
